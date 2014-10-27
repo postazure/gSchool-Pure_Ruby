806dc471730991c58207f0845a39bd878e585036
postazure <gordonmaxc@gmail.com>
2014-10-27 14:36:37 -0700

    add ageist.rb

---
+++ ageist.rb
@@ -0,0 +1,52 @@
+
+# DO IT AS A CONJOINED IF STATEMENT
+# def ageist age
+#   if age <= 1
+#     status = "baby"
+#   elsif age > 1 && age <=10
+#     status = "child"
+#   elsif age >10 && age <=12
+#     status = "tween"
+#   elsif age >12 && age <=19
+#     status = "teenger"
+#   elsif age >19 && age <=40
+#     status = "adult"
+#   elsif age >40 && age <=65
+#     status = "middle-age"
+#   elsif age >65 && age <=100
+#     status = "senior"
+#   elsif age >100
+#     status = "old as .."
+#   end
+#
+#   return status
+# end
+
+#DO IT WITH RANGES
+def ageist age
+  if age <= 1
+    status = "baby"
+  elsif (1..10) === age
+    status = "child"
+  elsif (11..12) === age
+    status = "tween"
+  elsif (13..19) === age
+    status = "teenger"
+  elsif (20..40) === age
+    status = "adult"
+  elsif (41..65) === age
+    status = "middle-age"
+  elsif (66..100) === age
+    status = "senior"
+  elsif age >100
+    status = "old as .."
+  end
+
+  return status
+end
+
+
+num = rand(111)
+
+puts "The age of this person is #{num}."
+puts "This person is a #{ageist(num)}."
--- ruby_script.rb
+++ ruby_script.rb
@@ -1,1 +1,24 @@
+#--------- 10/27/14 Class notes------------------------------
 puts "I'm a ruby script."
+
+#---------Everything in Ruby is an object--------------------
+puts 4.even? # this is an object so i can call a method on it
+puts 3.2.round # same here
+puts 4.class
+puts 4.between?(3, 80)
+
+# syntax: thing.method
+
+
+# Note: '3.send :class' and '3.class' work the same, but don't
+# use it this way. It would be dumb.
+
+# If you do 3.methods it will show ALL the methods that can be
+# called on it. ('<object>.methods - Object.new.methods' will
+# show you everything specific to that object).
+
+# Other langs have Primitives, they can not have methods
+# called on them. In Java, 4 is just a 4 (int)
+
+#-------- Variables -----------------------------------------
+people_currently_in_class = 27 # this is a local variable
